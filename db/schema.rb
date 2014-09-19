7e52f044650f85ca6fe5dbe5570b2cea982c2d7e
Yev Gurevich <yev2000@verizon.net>
2014-09-19 13:53:58 -0400

    created 1:M association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919172524) do
+ActiveRecord::Schema.define(version: 20140919172832) do
 
   create_table "posts", force: true do |t|
     t.string   "url"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140919172524) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
