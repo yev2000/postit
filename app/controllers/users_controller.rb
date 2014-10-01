649d18fd432f7ed393061021a361135b8d8803d6
Yev Gurevich <yev2000@verizon.net>
2014-09-30 23:59:08 -0400

    some formatting changes after changing sublime tabstops

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,29 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :posts, :show, :update]
-	before_action :require_logged_in_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :posts, :show, :update]
+  before_action :require_logged_in_user, only: [:edit, :update]
 
 	def new
 		@user = User.new
 	end
 
 	def create
-	    @user = User.new(user_params)
+    @user = User.new(user_params)
     
-	    if @user.save
-	      flash[:notice] = "Your user #{@user.username} was created."
+    if @user.save
+      flash[:notice] = "Your user #{@user.username} was created."
 
-	      # this is a bit of a hack, but we redirect to the posts index rather than a (nonexistent)
-	      # users index.
+      # this is a bit of a hack, but we redirect to the posts index rather than a (nonexistent)
+      # users index.
 
 
-	      # if we want to log the user in, we simply create
-	      # a session for the user implicitly.
-	      session[:userid] = @user.id
+      # if we want to log the user in, we simply create
+      # a session for the user implicitly.
+      session[:userid] = @user.id
 
-	      redirect_to posts_path
-	    else
-	      render :new
-	    end
+      redirect_to posts_path
+    else
+      render :new
+    end
 	end
 
 	def edit
@@ -45,11 +45,11 @@ class UsersController < ApplicationController
 
 		# (will validations make sure duplicate username is not set?)
 		if @user.save
-      		flash[:notice] = "The user \"#{@user.username}\" was updated."
-      		redirect_to root_path
-    	else
-      		render :edit
-      	end
+      flash[:notice] = "The user \"#{@user.username}\" was updated."
+      redirect_to root_path
+    else
+    	render :edit
+    end
     	
 	end
 
@@ -80,25 +80,25 @@ class UsersController < ApplicationController
 			# here we are looking for any users matching the search expression		
 
 			# The original approach used User.all.each to iterate over all of the users.
-      		# But this is very inefficient if the system has thousands of users since all
-      		# will be read into memory.
-      		# The more efficient approach pointed out by Brandon C. is to use
-      		# matching_users = <class>.where("<field> LIKE #{params[:uname].gsub('*', '%').gsub('?', '_')}")
-      		# and furthermore, to protect against SQL injection, it would be
-      		# matching_posts = <class>.where("<field> LIKE ?", params[:uname].gsub('*', '%').gsub('?', '_'))
-      		@search_results = User.where("username LIKE ?", params[:uname].gsub('*', '%').gsub('?', '_'))
-      		
-      		# If we are also looking for users with at least one post, we need to iterate over the search
-      		# results and cull the resulting set by removing ones without any posts.
-	      	if @with_posts_val == "1"
-          		@search_results.delete_if do |u|
-            		if u.posts.length > 0
-              			false
-              		else
-            			true
-              	    end # comments length
-          		end #delete_if
-        	end #comments_with_val == 1
+    	# But this is very inefficient if the system has thousands of users since all
+    	# will be read into memory.
+    	# The more efficient approach pointed out by Brandon C. is to use
+    	# matching_users = <class>.where("<field> LIKE #{params[:uname].gsub('*', '%').gsub('?', '_')}")
+    	# and furthermore, to protect against SQL injection, it would be
+    	# matching_posts = <class>.where("<field> LIKE ?", params[:uname].gsub('*', '%').gsub('?', '_'))
+      @search_results = User.where("username LIKE ?", params[:uname].gsub('*', '%').gsub('?', '_'))
+  		
+  		# If we are also looking for users with at least one post, we need to iterate over the search
+  		# results and cull the resulting set by removing ones without any posts.
+    	if @with_posts_val == "1"
+    		@search_results.delete_if do |u|
+      		if u.posts.length > 0
+       			false
+       		else
+      			true
+     	    end # comments length
+    		end #delete_if
+    	end #comments_with_val == 1
 
 			# now have to consider how to sort
 			
@@ -120,29 +120,29 @@ class UsersController < ApplicationController
 
 	private
 
-    def user_params
-      params.require(:user).permit(:username, :password)
-    end
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 
-    def set_current_user
-    	@user = current_user_get
-    end
+  def set_current_user
+  	@user = current_user_get
+  end
 
-    def set_user
-      begin
-        @user = User.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-        flash[:notice] = "There is no user with ID #{params[:id]}.  Showing user search instead." 
-        redirect_to search_users_path
-      end
+  def set_user
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:notice] = "There is no user with ID #{params[:id]}.  Showing user search instead." 
+      redirect_to search_users_path
     end
+  end
 
-    def require_logged_in_user
+  def require_logged_in_user
 		if @user != current_user_get
-			flash[:error] = "You cannot edit a different user's profile"
-			redirect_to root_path
-		end
-    end
+ 		  flash[:error] = "You cannot edit a different user's profile"
+ 		  redirect_to root_path
+	  end
+  end
 
 end
 
