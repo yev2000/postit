58beeb51c8dcbb1adc67bf6d3bdbf7b7fa05ce3b
Yev Gurevich <yev2000@verizon.net>
2014-10-06 22:59:32 -0400

    minor styling changes; make edit categories link not appear if not admin user logged in

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
 	before_action :set_category, only: [:show, :edit, :update]
 	before_action :require_user, except: [:index, :show]
-	before_action :require_admin, only: [:new, :create]
+	before_action :require_admin, only: [:new, :edit, :create]
 
 	def show
     respond_to do |format|
